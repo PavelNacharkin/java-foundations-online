8aea933ad5f710695eab0e9276aeaa52fe179d55
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-10-27 23:07:30 +0300

    Not

--- src/main/java/ru/itsjava/junit/Person.java
+++ src/main/java/ru/itsjava/junit/Person.java
@@ -1,4 +1,11 @@
 package ru.itsjava.junit;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
+    private final String name;
+    private int age;
 }
