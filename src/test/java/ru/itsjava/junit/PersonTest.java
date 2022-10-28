132e49fac0843893a0b10f4fe3eca53e5555bc9f
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-10-28 19:34:40 +0300

    Not

--- src/test/java/ru/itsjava/junit/PersonTest.java
+++ src/test/java/ru/itsjava/junit/PersonTest.java
@@ -1,4 +1,9 @@
 package ru.itsjava.junit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.itsjava.Watch;
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Класс Person должен")
 
 public class PersonTest {
 }
