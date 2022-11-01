a55665fc602db6e3a9b89ebf3cad476a77488554
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-01 22:29:44 +0300

    Сorrected the code

--- src/test/java/ru/itsjava/junit/PersonTest.java
+++ src/test/java/ru/itsjava/junit/PersonTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 public class PersonTest {
     public static final String DEFAULT_NAME = "Pavel";
     public static final int DEFAULT_AGE = 18;
+    private static final String NEW_NAME ="Garry" ;
+    private static final int NEW_AGE = 17;
 
     @Test
     @DisplayName("корректно создаваться конструктором ")
-    public void shouldHaveCorrectConstrucrtor() {
+    public void shouldHaveCorrectConstructor() {
         Person actualPerson = new Person(DEFAULT_NAME, DEFAULT_AGE);
         Assertions.assertEquals(DEFAULT_NAME, actualPerson.getName());
         Assertions.assertEquals(DEFAULT_AGE, actualPerson.getAge());
@@ -29,11 +31,10 @@ public class PersonTest {
     @DisplayName("корректно проверять совершеннолетие ")
     public void shouldCorrectLegalAge() {
         Person actualPerson = new Person(DEFAULT_NAME, DEFAULT_AGE);
-        actualPerson.birthday();
-        if (actualPerson.getAge() >= 18) {
-            Assertions.assertEquals(actualPerson.takeBeer(), true);
-        } else if (actualPerson.getAge() < 18) {
-            Assertions.assertEquals(actualPerson.takeBeer(), false);
-        }
+        Person newPerson = new Person(NEW_NAME,NEW_AGE);
+        Assertions.assertTrue(actualPerson.takeBeer());
+        Assertions.assertFalse(newPerson.takeBeer());
+        newPerson.birthday();
+        Assertions.assertTrue(newPerson.takeBeer());
     }
 }
