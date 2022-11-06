6dad4305f6f8435bd1eb5b23beb4304d761334c9
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-06 22:46:56 +0300

    practice

---
+++ src/main/java/ru/itsjava/interfaces/InterfacesPractice.java
@@ -0,0 +1,10 @@
+package ru.itsjava.interfaces;
+
+public class InterfacesPractice {
+    public static void main(String[] args) {
+        Creatable khotabich = new Gin();
+        khotabich.createWish();
+        Creatable badGin= new BadGin();
+        badGin.createWish();
+    }
+}
