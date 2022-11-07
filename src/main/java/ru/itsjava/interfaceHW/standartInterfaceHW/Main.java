df24102739f804a2759b3f76369ecc7d6333e6f0
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-07 23:13:26 +0300

    practice

---
+++ src/main/java/ru/itsjava/interfaceHW/standartInterfaceHW/Main.java
@@ -0,0 +1,7 @@
+package ru.itsjava.interfaceHW.standartInterfaceHW;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/itsjava/interfaceHW/standartInterfaceHW/Person.java
@@ -0,0 +1,5 @@
+package ru.itsjava.interfaceHW.standartInterfaceHW;
+
+public class Person {
+
+}
