c7c9ec9e46069a6f6be23386e83e1dc168f26045
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-10 22:08:24 +0300

    practice

---
+++ src/main/java/ru/itsjava/interfaces/AbstractPractice.java
@@ -0,0 +1,7 @@
+package ru.itsjava.interfaces;
+
+public class AbstractPractice {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/itsjava/interfaces/Car.java
@@ -0,0 +1,13 @@
+package ru.itsjava.interfaces;
+
+public abstract class Car {
+
+    abstract void getCarMake();
+
+
+
+
+    public void seatInTheCar() {
+        System.out.println("So good seats!;)");
+    }
+}
