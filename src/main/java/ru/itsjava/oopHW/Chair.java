1856a070192e55b379c385dc231d8e9f463f679a
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-10 22:14:39 +0300

    practice

--- src/main/java/ru/itsjava/oopHW/Chair.java
+++ src/main/java/ru/itsjava/oopHW/Chair.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class Chair {
+public abstract class Chair {
     private final int maxLoad;
     private final int amountSupportPoint;
     private String sheathingMaterial;
