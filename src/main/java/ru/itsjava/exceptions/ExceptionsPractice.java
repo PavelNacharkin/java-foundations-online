17d6a1da3077de04ff7f28cf0f2df7b53ac8d0a9
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2022-11-18 18:55:22 +0300

    practice

---
+++ src/main/java/ru/itsjava/exceptions/ExceptionsPractice.java
@@ -0,0 +1,10 @@
+package ru.itsjava.exceptions;
+
+public class ExceptionsPractice {
+    public static void main(String[] args) {
+
+
+        throw new RuntimeException("My runtime exceptions");
+
+    }
+}
