b97c052938d9a407e5cc8db98c8024b6adf6fc28
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2023-05-26 01:00:20 +0300

    Add Russian method name

--- src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
+++ src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
@@ -3,48 +3,48 @@ package ru.itsjava.annotations.junit;
 import ru.itsjava.annotations.junit.annotatoins.*;
 
 public class MyFavoriteTest {
-    @DisplayName(testMessage = "Method before all tests")
+    @DisplayName(testMessage = "Метод перед всеми тестами")
     @BeforeAll
     public void beforeAllMethod() {
         System.out.println("BEFORE ALL");
     }
 
-    @DisplayName(testMessage = "Method before each test")
+    @DisplayName(testMessage = "Метод перед каждым тестом")
     @Before
     public void beforeMethod() {
         System.out.println("BEFORE");
     }
 
-    @DisplayName(testMessage = "Test #1")
+    @DisplayName(testMessage = "Тест №1")
     @Test
     public void test1() {
     }
 
-    @DisplayName(testMessage = "Test #2")
+    @DisplayName(testMessage = "Тест №2")
     @Test
     public void test2() {
     }
 
-    @DisplayName(testMessage = "Test #3")
+    @DisplayName(testMessage = "Тест №3")
     @Test
     public void test3() {
 
         throw new RuntimeException();
     }
 
-    @DisplayName(testMessage = "Test #4")
+    @DisplayName(testMessage = "Тест №4")
     @Test
     public void test4() {
         throw new RuntimeException();
     }
 
-    @DisplayName(testMessage = "Method after each test")
+    @DisplayName(testMessage = "Метод после каждого теста")
     @After
     public void afterMethod() {
         System.out.println("AFTER");
     }
 
-    @DisplayName(testMessage = "Method after all tests")
+    @DisplayName(testMessage = "Метод после всех тестов")
     @AfterAll
     public void afterAllMethod() {
         System.out.println("AFTER All");
